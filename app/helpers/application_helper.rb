1a59a1a98ea276bb87ce85285ca3f3e3248778e2
Igor Dragun <igor_dragun@bk.ru>
2022-07-13 10:08:38 +0300

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return full title on base page title
+  def full_title(page_title = '')
+    base_title = "«Help’s on the way"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
