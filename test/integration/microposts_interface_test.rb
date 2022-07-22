652897de856e6147035df64158948720eb43824f
Igor Dragun <igor_dragun@bk.ru>
2022-07-22 15:29:54 +0300

    test-exercises

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,16 +9,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select "div.pagination"
+    assert_select "input[type=file]"
     assert_no_difference "Micropost.count" do
       post microposts_path, params: { micropost: { content: ""}}
     end
     assert_select "div#error_explanation"
     # assert_select "a[href=?]", "/?page=2"
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/sea.png', 'image/png')
     assert_difference "Micropost.count", 1 do
-      post microposts_path, params: { micropost: {content: content}}
+      post microposts_path, params: { micropost: { content: content, picture: picture }}
     end
-    assert_redirected_to root_url
+    assert assigns(:micropost).picture?
     follow_redirect!
     assert_match content, response.body
     assert_select "a", text: "delete"
@@ -28,5 +30,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     end
     get user_path(users(:archer))
     assert_select "a", text: "delete", count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
